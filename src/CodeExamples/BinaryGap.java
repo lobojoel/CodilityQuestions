3d97d6a27411510e257bb2e0e3df0cb5cbcf2a14
Joel Lobo <joelnishan@gmail.com>
2013-06-24 21:02:48 -0400

    Update BinaryGap.java

--- src/CodeExamples/BinaryGap.java
+++ src/CodeExamples/BinaryGap.java
@@ -10,25 +10,25 @@ public class BinaryGap {
 	 // BINARY GAP
 	  public int getBinaryGap(int number)
 	  {
-		  long previous = 0;
-		  long current = 0;
-		  long longest = 0;
+		  int previous = 0;
+		  int current = 0;
+		  int longest = 0;
 		  
-		  if(number<=0)
-		  {System.out.println("RETURN 0"); return 0;}
+		  if(number<0)
+		  {System.out.println("Please Enter a positive number."); return 0;}
 		  		  
 		  do{
 			  previous= number & 1;
 			  number = number>>1;
-	        if(previous==0)
-	        {current++;}
-	        else{
-	        	if(current>longest)
-	        	{longest = current;}
-	        	current=0;
-	        }
+	       		  if(previous==0)
+	        	  {current++;}
+	       		   else{
+	        		if(current>longest)
+	        		{longest = current;}
+	        		current=0;
+	        	  }
 		  }while(number>0);
 			  
-			  return (int)Math.max(current, longest);
+		  return Math.max(current, longest);
 	  }
 }
